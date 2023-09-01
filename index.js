a6c2ff89588926fbfe95bccba2e9bda5330521f5
Arno <arno.pimenta@gmail.com>
2023-09-01 19:55:10 +0200

    MusicBooth

---
+++ index.js
@@ -0,0 +1,22 @@
+const image = document.getElementById('cover'),
+    title = document.getElementById('music-title'),
+    artist = document.getElementById('music-artist'),
+    currentTimeEl = document.getElementById('current-time'),
+    durationEl = document.getElementById('duration'),
+    progress = document.getElementById('progress'),
+    playerProgress = document.getElementById('player-progress'),
+    prevBtn = document.getElementById('prev'),
+    nextBtn = document.getElementById('next'),
+    playBtn = document.getElementById('play'),
+    background = document.getElementById('bg-img');
+
+
+const music = new Audio();
+
+const songs = [
+    {
+        path:'assets1.mp3',
+        displayName: NameSong,
+        cover:''
+    }
+]
